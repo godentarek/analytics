acf019a47bc85c98b38609339ac9ef26be7a3709
godentarek <eng.tarek.cs@gmail.com>
2016-06-16 09:10:53 -0400

    Applied fixes from StyleCI

--- Config/AnalyticsActivation.php
+++ Config/AnalyticsActivation.php
@@ -1,34 +1,35 @@
 <?php
 
-class AnalyticsActivation {
+class AnalyticsActivation
+{
+    public function beforeActivation($controller)
+    {
+        return true;
+    }
 
-	public function beforeActivation($controller) {
-		return true;
-	}
+    public function onActivation($controller)
+    {
+        $controller->Setting->write('Site.Analytics.webPropertyId', '', [
+            'editable'    => 1,
+            'title'       => 'Web Property ID',
+            'description' => 'Enter your site Web Property ID',
+            ]);
 
-	public function onActivation($controller) {
+        $controller->Setting->write('Site.Analytics.domain', '', [
+            'editable'    => 1,
+            'title'       => 'Primary Domain',
+            'description' => 'If you\'re using multiple subdomains, enter your primary domain name here',
+            ]);
+    }
 
-		$controller->Setting->write('Site.Analytics.webPropertyId', '', array(
-			'editable' => 1,
-			'title' => 'Web Property ID',
-			'description' => 'Enter your site Web Property ID',
-			));
+    public function beforeDeactivation($controller)
+    {
+        return true;
+    }
 
-		$controller->Setting->write('Site.Analytics.domain', '', array(
-			'editable' => 1,
-			'title' => 'Primary Domain',
-			'description' => 'If you\'re using multiple subdomains, enter your primary domain name here',
-			));
-
-	}
-
-	public function beforeDeactivation($controller) {
-		return true;
-	}
-
-	public function onDeactivation($controller) {
-		$controller->Setting->deleteKey('Site.Analytics.webPropertyId');
-		$controller->Setting->deleteKey('Site.Analytics.domain');
-	}
-
+    public function onDeactivation($controller)
+    {
+        $controller->Setting->deleteKey('Site.Analytics.webPropertyId');
+        $controller->Setting->deleteKey('Site.Analytics.domain');
+    }
 }
